ad1883a1bcec6344535e21d6bb1d29264d8377ce
bd-dev-ismail <ismailratul7@gmail.com>
2023-05-12 16:39:37 +0600

    folder structer

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,22 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const port = 5000;
+import mongoose from "mongoose";
+import app from "./app";
+const port: number = 5000;
+
 
 //Data base connection
 async function bootStrap() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/pratices-mongoose");
 
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     console.log("Data Base Connection Successful");
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
   } catch (err) {
     console.log("Failed to connect data base", err);
   }
 }
 bootStrap();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+
+
